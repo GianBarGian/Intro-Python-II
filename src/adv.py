72322cca8133bd7357ae3134678e6e2eb35bc839
GianBarGian <giacomobenati@hotmail.it>
2019-10-02 16:16:06 +0200

    done with MVP for day2

--- src/adv.py
+++ src/adv.py
@@ -64,32 +64,64 @@ player = Player(name, room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-move = ''
-while move != 'q':
-    if move != "no repeat":
+move = [""]
+while move[0] != 'q':
+    if move[0] != "no repeat":
         print(f'\n{player.name} you are at {player.room.name}\n')
         print(player.room.description)
-        print('\nAfter you searched the room you could find these items')
-        for item in player.room.items:
-            print(f'  -{item.name}: This is {item.description}')
     move = input('\nWhere do you want to go? n/s/w/e (q for quit the game): ')
-    if move == 'q':
-        print('Thanks for playing this game')
-    elif move != 'n' and move != 's' and move != 'w' and move != 'e':
-        print('Pease insert a valid direction (or q to quit)')
-        move = "no repeat"
-    elif move == 'n' and player.room.n_to:
-        player.room = player.room.n_to
-        print('\nYou moved north')
-    elif move == 's' and player.room.s_to:
-        player.room = player.room.s_to
-        print('\nYou moved south')
-    elif move == 'e' and player.room.e_to:
-        player.room = player.room.e_to
-        print('\nYou moved east')
-    elif move == 'w' and player.room.w_to:
-        player.room = player.room.w_to
-        print('\nYou moved west')
+    move = move.strip().split(' ')
+    if len(move) == 1:
+        if move[0] == 'q':
+            print('Thanks for playing this game')
+        elif move[0] != 'n' and move[0] != 's' and move[0] != 'w' and move[0] != 'e' and move[0] != 'inspect' and move[0] != 'inventory' and move[0] != 'i' :
+            print('Pease insert a valid direction (or q to quit)')
+            move[0] = "no repeat"
+        elif move[0] == 'n' and player.room.n_to:
+            player.room = player.room.n_to
+            print('\nYou moved north')
+        elif move[0] == 's' and player.room.s_to:
+            player.room = player.room.s_to
+            print('\nYou moved south')
+        elif move[0] == 'e' and player.room.e_to:
+            player.room = player.room.e_to
+            print('\nYou moved east')
+        elif move[0] == 'w' and player.room.w_to:
+            player.room = player.room.w_to
+            print('\nYou moved west')
+        elif move[0] == 'inspect':
+            player.room.inspect()
+            move = "no repeat"
+        elif move[0] == 'i' or move[0] == 'inventory':
+            print('Inventory:')
+            for item in player.inventory:
+                print(f'  -{item.name}')
+            move[0] = "no repeat"
+        else:
+            print('\nThere is nothing there, try another direction')
+            move[0] = "no repeat"
     else:
-        print('\nThere is nothing there, try another direction')
-        move = "no repeat"
+        if move[0] == 'take':
+            item_exist = list(filter(lambda item: item.name == move[1], player.room.items))
+            if len(item_exist) > 0:
+                item = item_exist[0]
+                player.room.items.remove(item)
+                player.inventory.append(item)
+                item.on_take(item.name)
+            else:
+                print('That item is not here!')
+        if move[0] == 'drop':
+            item_exist = list(filter(lambda item: item.name == move[1], player.inventory))
+            if len(item_exist) > 0:
+                item = item_exist[0]
+                player.room.items.append(item)
+                player.inventory.remove(item)
+                item.on_drop(item.name)
+            else:
+                print('That item is not here!')
+        move[0] = "no repeat"
+        
+
+
+                    
+
