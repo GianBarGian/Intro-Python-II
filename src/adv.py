2831c01760c7b1e73818435cc91ed41f1bc2278b
GianBarGian <giacomobenati@hotmail.it>
2019-10-03 14:23:53 +0200

    refactored main loop + added Light_source calss, and implemented in the game

--- src/adv.py
+++ src/adv.py
@@ -1,18 +1,19 @@
 from room import Room
 from player import Player
 from item import Item
+from light_source import Light_source
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", True),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", True),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", True),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
@@ -30,6 +31,7 @@ shield = Item('Shield', 'a shield to protect yourself')
 boots = Item('Boots', 'a pair of boots to protect yourself from cold')
 ring = Item('Ring', 'a magic ring. What will it do?')
 armor = Item('Armor', 'a fine armor to protect yourself')
+torch = Light_source('Torch', 'a torch to see in the dark')
 
 # Link rooms together
 
@@ -48,67 +50,96 @@ room['outside'].items = [hat, boots]
 room['foyer'].items = [armor]
 room['overlook'].items = [ring]
 room['narrow'].items = [sword, shield]
+room['treasure'].items = [torch]
 
+
+act = [""]
+
+
+def action_logic(action_list):
+    if len(action_list) == 1:
+        single_action(action_list[0])
+    elif len(action_list) == 2:
+        composite_action(action_list)
+
+
+def single_action(action):
+    global act
+    if action == 'q':
+        print('Thanks for playing this game')
+    elif action not in ['n', 's', 'w', 'e', 'inspect', 'inventory', 'i']:
+        print('Pease insert a valid direction (or q to quit)')
+        act[0] = "no repeat"
+    elif action == 'n' and player.room.n_to:
+        player.room = player.room.n_to
+        print('\nYou moved north')
+    elif action == 's' and player.room.s_to:
+        player.room = player.room.s_to
+        print('\nYou moved south')
+    elif action == 'e' and player.room.e_to:
+        player.room = player.room.e_to
+        print('\nYou moved east')
+    elif action == 'w' and player.room.w_to:
+        player.room = player.room.w_to
+        print('\nYou moved west')
+    elif action == 'inspect':
+        player.room.inspect()
+        act[0] = "no repeat"
+    elif action == 'i' or action == 'inventory':
+        print('Inventory:')
+        for item in player.inventory:
+            print(f'  -{item.name}')
+        act[0] = "no repeat"
+    else:
+        print('\nThere is nothing there, try another direction')
+        act[0] = "no repeat"
+
+
+def composite_action(action_list):
+    global act
+    if action_list[0] == 'take':
+        item_exist = list(
+            filter(lambda item: item.name == action_list[1], player.room.items))
+        if len(item_exist) > 0:
+            item = item_exist[0]
+            player.room.items.remove(item)
+            player.inventory.append(item)
+            item.on_take(item.name)
+        else:
+            print('That item is not here!')
+    if action_list[0] == 'drop':
+        item_exist = list(
+            filter(lambda item: item.name == action_list[1], player.inventory))
+        if len(item_exist) > 0:
+            item = item_exist[0]
+            player.room.items.append(item)
+            player.inventory.remove(item)
+            item.on_drop(item.name)
+        else:
+            print('That item is not here!')
+    act[0] = "no repeat"
+
+def room_description(player):
+    is_light_source = list(filter(lambda item: type(item).__name__ == "Light_source", player.room.items))
+    if player.room.is_light or len(is_light_source) > 0:
+        print(f'\n{player.name} you are at {player.room.name}\n')
+        print(player.room.description)
+    else:
+        print('Is pitch black!')
+
+print(type(torch).__name__)
 #
 # Main
 #
+
+
 name = input('Please provide a name for your character: ')
 player = Player(name, room['outside'])
-move = [""]
-while move[0] != 'q':
-    if move[0] != "no repeat":
-        print(f'\n{player.name} you are at {player.room.name}\n')
-        print(player.room.description)
-    move = input('\nWhere do you want to go? n/s/w/e (q for quit the game): ')
-    move = move.strip().split(' ')
-    if len(move) == 1:
-        if move[0] == 'q':
-            print('Thanks for playing this game')
-        elif move[0] != 'n' and move[0] != 's' and move[0] != 'w' and move[0] != 'e' and move[0] != 'inspect' and move[0] != 'inventory' and move[0] != 'i':
-            print('Pease insert a valid direction (or q to quit)')
-            move[0] = "no repeat"
-        elif move[0] == 'n' and player.room.n_to:
-            player.room = player.room.n_to
-            print('\nYou moved north')
-        elif move[0] == 's' and player.room.s_to:
-            player.room = player.room.s_to
-            print('\nYou moved south')
-        elif move[0] == 'e' and player.room.e_to:
-            player.room = player.room.e_to
-            print('\nYou moved east')
-        elif move[0] == 'w' and player.room.w_to:
-            player.room = player.room.w_to
-            print('\nYou moved west')
-        elif move[0] == 'inspect':
-            player.room.inspect()
-            move[0] = "no repeat"
-        elif move[0] == 'i' or move[0] == 'inventory':
-            print('Inventory:')
-            for item in player.inventory:
-                print(f'  -{item.name}')
-            move[0] = "no repeat"
-        else:
-            print('\nThere is nothing there, try another direction')
-            move[0] = "no repeat"
-    else:
-        if move[0] == 'take':
-            item_exist = list(
-                filter(lambda item: item.name == move[1], player.room.items))
-            if len(item_exist) > 0:
-                item = item_exist[0]
-                player.room.items.remove(item)
-                player.inventory.append(item)
-                item.on_take(item.name)
-            else:
-                print('That item is not here!')
-        if move[0] == 'drop':
-            item_exist = list(
-                filter(lambda item: item.name == move[1], player.inventory))
-            if len(item_exist) > 0:
-                item = item_exist[0]
-                player.room.items.append(item)
-                player.inventory.remove(item)
-                item.on_drop(item.name)
-            else:
-                print('That item is not here!')
-        move[0] = "no repeat"
+while act[0] != 'q':
+    if act[0] != "no repeat":
+        room_description(player)
+        # print(f'\n{player.name} you are at {player.room.name}\n')
+        # print(player.room.description)
+    act = input('\nWhere do you want to go? n/s/w/e (q for quit the game): ')
+    act = act.strip().split(' ')
+    action_logic(act)
--- src/room.py
+++ src/room.py
@@ -3,9 +3,10 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, is_light = False):
         self.name = name
         self.description = description
+        self.is_light = is_light 
         self.n_to = None
         self.s_to = None
         self.e_to = None
